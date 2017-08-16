6dac1b3e67937e97531a785b05229b0303076a35
Jelena Nesic <jelenanesicm@gmail.com>
2017-08-16 13:25:09 +0200

    First commit

---
+++ src/Clock.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Clock extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: '4',
+      hours: '3',
+      minutes: '33',
+      seconds: '33'
+    }
+  }
+
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
+  }
+
+  componentDidMount() {
+    //live update clock
+    setInterval(() => this.getTimeUntil(this.props.deadline), 1000); //1s
+  }
+
+  getTimeUntil(deadline) {
+    //time = deadline - current time
+    const time = Date.parse(deadline) - Date.parse(new Date());
+
+    //Math.floor()
+    const seconds = Math.floor((time/1000) % 60);
+    const minutes = Math.floor((time/1000/60) % 60);
+    const hours = Math.floor(time/(1000*60*60)% 24);
+    const days = Math.floor(time/(1000*60*60*24));
+
+    //from App.js ---> get date
+    // this.setState({days: days, hours: hours, minutes: minutes, seconds: seconds});
+    this.setState({days, hours, minutes, seconds});
+  }
+
+  render() {
+    return (
+      <div className="clock-div clearfix">
+        <p className="clock-days">{this.state.days} days</p>
+        <p className="clock-hours">{this.state.hours} hours</p>
+        <p className="clock-minutes">{this.state.minutes} minutes</p>
+        <p className="clock-seconds">{this.state.seconds} seconds</p>
+      </div>
+    );
+  }
+}
+
+export default Clock;
